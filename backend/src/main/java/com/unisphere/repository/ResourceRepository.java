4c27f12f2e3d1427ac3a72813ad1e3bcfb8bb7d4
Pushpika Ishan <pushpikaishan@gmail.com>
2026-04-26 12:29:21 +0530

    flix
    flix

--- backend/src/main/java/com/unisphere/repository/ResourceRepository.java
+++ backend/src/main/java/com/unisphere/repository/ResourceRepository.java
@@ -4,6 +4,7 @@ import com.unisphere.entity.Resource;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
+
 public interface ResourceRepository extends JpaRepository<Resource, Long>, JpaSpecificationExecutor<Resource> {
     boolean existsByNameIgnoreCase(String name);
 
--- backend/src/main/java/com/unisphere/service/ResourceService.java
+++ backend/src/main/java/com/unisphere/service/ResourceService.java
@@ -4,6 +4,7 @@ import com.unisphere.dto.resource.ResourceCreateDTO;
 import com.unisphere.dto.resource.ResourceResponseDTO;
 import com.unisphere.dto.resource.ResourceStatusUpdateDTO;
 import com.unisphere.dto.resource.ResourceUpdateDTO;
+
 import com.unisphere.entity.ResourceStatus;
 import com.unisphere.entity.ResourceType;
 import org.springframework.data.domain.Page;
@@ -11,6 +12,7 @@ import org.springframework.data.domain.Page;
 public interface ResourceService {
 
     Page<ResourceResponseDTO> getResources(
+
         ResourceType type,
         Integer minCapacity,
         String location,
@@ -18,6 +20,7 @@ public interface ResourceService {
         String search,
         int page,
         int size
+
     );
 
     ResourceResponseDTO getResourceById(Long id);
@@ -29,4 +32,5 @@ public interface ResourceService {
     void deleteResource(Long id);
 
     ResourceResponseDTO updateStatus(Long id, ResourceStatusUpdateDTO request);
+
 }
